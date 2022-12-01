d66ef1a01da1a0925edbac7a9a5cb13f950d6c25
Carles Arnal <carlesarnal92@gmail.com>
2022-12-01 13:07:30 +0100

    Add integration tests for the quarkus extension

---
+++ quarkus-extension/integration-tests/src/test/java/io/apicurio/openapi/server/generator/it/PetStoreTest.java
@@ -0,0 +1,46 @@
+package io.apicurio.openapi.server.generator.it;
+
+import io.petstore.beans.Pet;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.anything;
+
+@QuarkusTest
+public class PetStoreTest {
+
+    @Test
+    public void testapi() {
+
+        Pet pet = new Pet();
+        pet.setName("test");
+        pet.setId(1234L);
+        pet.setStatus(Pet.Status.available);
+
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(pet)
+                .post("/pet")
+                .then()
+                .statusCode(204)
+                .body(anything());
+
+        final Pet returnedPet = given()
+                .when()
+                .accept(ContentType.JSON)
+                .get("/pet/1234")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Pet.class);
+
+        Assertions.assertEquals(pet.getId(), returnedPet.getId());
+        Assertions.assertEquals(pet.getName(), returnedPet.getName());
+        Assertions.assertEquals(pet.getStatus(), returnedPet.getStatus());
+    }
+}
