d0144aab7c6d090f2768590c07e686ec691e35c6
Eric Wittmann <eric.wittmann@gmail.com>
2023-06-05 12:48:37 -0400

    Fixed some failing tests

--- core/src/test/resources/OpenApi2JaxRsTest/_expected-registry-api-v2-nodates/generated-api/src/main/java/org/example/api/SearchResource.java
+++ core/src/test/resources/OpenApi2JaxRsTest/_expected-registry-api-v2-nodates/generated-api/src/main/java/org/example/api/SearchResource.java
@@ -1,13 +1,13 @@
 package org.example.api;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import java.io.InputStream;
 import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import org.example.api.beans.ArtifactSearchResults;
 import org.example.api.beans.SortBy;
 import org.example.api.beans.SortOrder;
